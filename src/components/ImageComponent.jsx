89d3ec2faf117491ab7ac14124481e6c4c9bc46b
Rotha Mom <rothamom22@gmail.com>
2024-06-07 22:45:21 +0700

    react-js

---
+++ src/components/ImageComponent.jsx
@@ -0,0 +1,12 @@
+
+import styles from '../styles/Image.module.css'
+const ImageComponet = () => {
+    return(
+        <>
+        <h1 className={styles.heading} >Hello image</h1>
+        <img src="https://i.pinimg.com/236x/0c/11/82/0c1182d5b237ea83402f66b08d1a18af.jpg" alt="" />
+
+        </>
+    )
+}
+export default ImageComponet;
